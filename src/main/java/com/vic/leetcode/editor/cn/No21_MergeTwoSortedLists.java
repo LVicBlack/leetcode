fad38fa906f640522d0aeaa3cd67acf7567346aa
vic <1173646430@qq.com>
2022-02-03 01:35:58 +0800

    fix: No21_MergeTwoSortedLists添加遍历解法

--- src/main/java/com/vic/leetcode/editor/cn/No21_MergeTwoSortedLists.java
+++ src/main/java/com/vic/leetcode/editor/cn/No21_MergeTwoSortedLists.java
@@ -40,22 +40,50 @@ package com.vic.leetcode.editor.cn;
 public class No21_MergeTwoSortedLists {
     public static void main(String[] args) {
         Solution solution = new No21_MergeTwoSortedLists().new Solution();
+        int[] ints1 = new int[]{1, 4, 5};
+        int[] ints2 = new int[]{1, 2, 3, 4, 6};
+        ListNode listNode1 = ListNode.convertArray(ints1);
+        ListNode listNode2 = ListNode.convertArray(ints2);
+        ListNode listNode = solution.mergeTwoLists(listNode1, listNode2);
+
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
+
+    // 迭代
     class Solution {
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if(l1 ==null){
+            ListNode dummy = new ListNode(-100);
+            ListNode cur = dummy;
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+            }
+            cur.next = l1 == null ? l2 : l1;
+            return dummy.next;
+        }
+    }
+
+    // 递归
+    class Solution1 {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
                 return l2;
             }
-            if(l2 == null){
+            if (l2 == null) {
                 return l1;
             }
-            if(l1.val<l2.val){
-                l1.next = mergeTwoLists(l1.next,l2);
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
                 return l1;
-            }else{
-                l2.next = mergeTwoLists(l1,l2.next);
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
                 return l2;
             }
         }
@@ -85,6 +113,16 @@ public class No21_MergeTwoSortedLists {
             } else {
                 return String.valueOf(this.val) + ", " + this.next.toString();
             }
+        }
+
+        static ListNode convertArray(int[] ints) {
+            ListNode dummy = new ListNode();
+            ListNode cur = dummy;
+            for (int i : ints) {
+                cur.next = new ListNode(i);
+                cur = cur.next;
+            }
+            return dummy.next;
         }
     }
 }
