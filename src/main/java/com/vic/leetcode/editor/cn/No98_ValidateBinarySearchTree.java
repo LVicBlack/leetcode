0d65279679127f027a433239d068054c97298e94
vic <1173646430@qq.com>
2022-02-06 00:27:43 +0800

    fix: No98_ValidateBinarySearchTree 递归

---
+++ src/main/java/com/vic/leetcode/editor/cn/No98_ValidateBinarySearchTree.java
@@ -0,0 +1,103 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+// 👍 1404 👎 0
+
+
+package com.vic.leetcode.editor.cn;
+
+public class No98_ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new No98_ValidateBinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    //TODO 中序遍历
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return true;
+        }
+    }
+
+    // 递归
+    class Solution1 {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        public boolean isValidBST(TreeNode root, long low, long high) {
+            if (root == null) return true;
+            int val = root.val;
+            if (val >= high || val <= low) return false;
+
+            return isValidBST(root.left, low, val) && isValidBST(root.right, val, high);
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
